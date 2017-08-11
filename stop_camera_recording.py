80c36a6cee26ed6d3a081ef6cc786ab568b19f34
Adam Short <adamshort1994@gmail.com>
2017-08-11 17:06:39 +0100

    switch to requests

--- stop_camera_recording.py
+++ stop_camera_recording.py
@@ -1,12 +1,10 @@
-from urllib2 import urlopen, Request
+import requests
 
 access_token = 'ac6d1e4e-6827-4924-ba0a-f5f5fcac85da'
 
 def start_stop(url):
-	request = Request(url)
-	# request.add_header('Authorization', 'token %s' % access_token)
-	response = urlopen(request)
-	print(response.read())
+	r = requests.get(url)
+	print(r.text)
 
 if __name__ == '__main__':
 	url = 'http://192.168.0.14:8124/Json/StartStopRecording?sourceId=0&enabled=false&authToken={0}'.format(access_token)
